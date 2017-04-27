ba512f756ee4bfb151ef4485f282c1376c666ec6
Carl-Philipp Harmant <cp.harmant@gmail.com>
2017-04-27 17:33:30 -0500

    first commit

---
+++ launch_all.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+sleep 2
+cd /home/carl/Documents/scripts/conky
+conky -dq -c time_rc
+conky -dq -c system_rc
+conky -dq -c processes_rc
+conky -dq -c memory_rc
+conky -dq -c filesystem_rc
+conky -dq -c network_rc
+
