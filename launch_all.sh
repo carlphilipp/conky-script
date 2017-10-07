3212a525edfdfd096788b62ed3b3733a91731021
Carl-Philipp Harmant <cp.harmant@gmail.com>
2017-10-07 13:12:33 -0500

    Merge all config to one config file and adapt to current laptop

--- launch_all.sh
+++ launch_all.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 sleep 2
 cd /home/carl/Documents/conky-script
-conky -dq -c time_rc
+#conky -dq -c time_rc
 conky -dq -c system_rc
-conky -dq -c processes_rc
-conky -dq -c memory_rc
-conky -dq -c filesystem_rc
-conky -dq -c network_rc
+#conky -dq -c processes_rc
+#conky -dq -c memory_rc
+#conky -dq -c filesystem_rc
+#conky -dq -c network_rc
